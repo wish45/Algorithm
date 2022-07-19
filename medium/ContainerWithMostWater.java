bfaee23debfd56e8c5d10b20714ddd99f2d3d561
newhyodong <hyodg90@gmail.com>
2022-07-19 15:56:22 +0900

    11. Container With Most Water

---
+++ medium/ContainerWithMostWater.java
@@ -0,0 +1,16 @@
+package medium;
+
+class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxValue = 0;
+
+        for(int i = 0; i<height.length-1; i++){
+            for(int j = i+1; j<height.length; j++){
+                int area = Math.min(height[i],height[j])*(j-i);
+                maxValue = Math.max(maxValue,area);
+            }
+        }
+        return maxValue;                            
+    }
+}
+
