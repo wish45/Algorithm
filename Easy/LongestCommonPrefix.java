fb73b6d96e7f6919a7da74275a523fd86a1d6d70
newhyodong <hyodg90@gmail.com>
2022-07-24 18:55:22 +0900

    14. Longest Common Prefix

---
+++ Easy/LongestCommonPrefix.java
@@ -0,0 +1,33 @@
+package Easy;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        int length = strs.length;
+        String maxStr = "";
+        int index=0;
+
+        if(length<1){
+            return maxStr;
+        }
+
+        while(true){
+            char ch;
+            if(strs[0].length()<=index){
+                return maxStr;
+            }
+            ch = strs[0].charAt(index);
+            for(int i=1; i<length; i++){
+                if(strs[i].length()<=index){
+                    return maxStr;
+                }
+                if(strs[i].charAt(index)!=ch){
+                    return maxStr;
+                }
+            }
+            index++;
+            maxStr+=ch;
+
+
+        }
+    }
+}
