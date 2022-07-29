54e3044b292ff199a76a3ab76b058e9a7f3952e6
newhyodong <hyodg90@gmail.com>
2022-07-29 18:37:23 +0900

    28. Implement strStr()

---
+++ Easy/ImplementStrStr.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        int index = 0;
+
+        if (haystack.contains(needle)) {
+            return haystack.indexOf(needle);
+        } else {
+            return -1;
+        }
+    }
+}
+
