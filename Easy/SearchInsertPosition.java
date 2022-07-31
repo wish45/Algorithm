1096031e74310af5065acf898d599ecd6ec8641a
newhyodong <hyodg90@gmail.com>
2022-07-31 18:08:04 +0900

    35. Search Insert Position

---
+++ Easy/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package Easy;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+        }
+
+        return start;
+
+    }
+}
