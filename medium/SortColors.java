e742a112d7ef46c16d2f396ef3b9533d2b8ef6d9
newhyodong <hyodg90@gmail.com>
2022-08-02 18:16:47 +0900

    75. Sort Colors

---
+++ medium/SortColors.java
@@ -0,0 +1,27 @@
+package medium;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int zeroC = 0;
+        int lastInt = nums.length - 1;
+        int tmp;
+        int index = 0;
+
+        while (zeroC <= lastInt) {
+            if (nums[zeroC] == 0) {
+                tmp = nums[zeroC];
+                nums[zeroC] = nums[index];
+                nums[index] = tmp;
+                zeroC++;
+                index++;
+            } else if (nums[zeroC] == 2) {
+                tmp = nums[zeroC];
+                nums[zeroC] = nums[lastInt];
+                nums[lastInt] = tmp;
+                lastInt--;
+            } else {
+                zeroC++;
+            }
+        }
+    }
+}
