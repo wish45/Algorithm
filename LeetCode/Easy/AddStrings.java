5e2af7af41182fdf6329adf59f49cdab24044f32
newhyodong <hyodg90@gmail.com>
2022-08-09 21:19:46 +0900

    415. Add Strings

---
+++ LeetCode/Easy/AddStrings.java
@@ -0,0 +1,26 @@
+package LeetCode.Easy;
+
+public class AddStrings {
+    public static void main(String args[]){
+        AddStrings asd =  new AddStrings();
+        System.out.println(asd.addStrings("6913259244","71103343"));
+
+    }
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length()-1, j=num2.length()-1;
+        int carry=0;
+        while(i>=0 || j>=0 || carry>0){
+            int n1=0, n2=0;
+            if(i>=0) n1 = num1.charAt(i)-'0';
+            if(j>=0) n2 = num2.charAt(j)-'0';
+
+            int sum = n1 + n2+ carry;
+            carry= sum/10;
+            sb.append(sum%10);
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
