0f1b62443c58d6417632d971d6fb34c4ff2de720
newhyodong <hyodg90@gmail.com>
2022-09-04 20:29:57 +0900

    cos pro test

---
+++ ThisIsCodingTest/CosPro1/Main_7.java
@@ -0,0 +1,34 @@
+package CosPro1;
+
+import java.util.Arrays;
+
+public class Main_7 {
+    public int[] solution(int[] arrA, int[] arrB) {
+        int arrA_idx = 0, arrB_idx = 0;
+        int arrA_len = arrA.length;
+        int arrB_len = arrB.length;
+        int answer[] = new int[arrA_len + arrB_len];
+        int answer_idx = 0;
+        while(arrB_len>arrB_idx && arrA_len>arrA_idx){
+            if(arrA[arrA_idx] < arrB[arrB_idx])
+                answer[answer_idx++] = arrA[arrA_idx++];
+            else
+                answer[answer_idx++] = arrB[arrB_idx++];
+        }
+        while(arrA_len>arrA_idx)
+            answer[answer_idx++] = arrA[arrA_idx++];
+        while(arrB_len>arrB_idx)
+            answer[answer_idx++] = arrB[arrB_idx++];
+        return answer;
+    }
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main_7 sol = new Main_7();
+        int[] arrA = {-2, 3, 5, 9};
+        int[] arrB = {0, 1, 5};
+        int[] ret = sol.solution(arrA, arrB);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+    }
+}
