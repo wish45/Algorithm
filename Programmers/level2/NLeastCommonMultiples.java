c6c7fd41dadc7d3a1e847e11643cbd3db434175b
newhyodong <hyodg90@gmail.com>
2022-11-12 17:08:00 +0900

    N개의 최소공배수

---
+++ Programmers/level2/NLeastCommonMultiples.java
@@ -0,0 +1,34 @@
+package level2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class NLeastCommonMultiples {
+    /**
+     * 배열이 주어졌을때 최소공배수 구하기
+     *
+     * */
+    public static void main(String args[]){
+        NLeastCommonMultiples asd = new NLeastCommonMultiples();
+        Integer[] arr = new Integer[]{2,6,8,14};
+        System.out.println(asd.solution(arr));
+    }
+    public int solution(Integer[] arr) {
+        int answer = arr[0];
+
+        for(int i=0; i<arr.length; i++){
+            answer = lcm(answer, arr[i]);
+        }
+        return answer;
+    }
+    static int gcd(int a, int b){
+        if(a%b == 0){
+            return b;
+        }
+
+        return gcd(b, a%b);
+    }
+    static int lcm(int a, int b){
+        return a*b / gcd(a,b);
+    }
+}
